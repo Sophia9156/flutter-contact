4f30dfa908bd23218651780d0e0e7152a7b0d1fd
Sophia9156 <suhyun9156@gmail.com>
2023-11-16 17:29:23 +0900

    feat: stateful widget으로 좋아요 기능 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,17 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   var a = 1;
+  var likes = [0, 0, 0];
+  var name = ["김영숙", "홍길동", "피자집"];
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +23,32 @@ class MyApp extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Text(a.toString()),
         onPressed: () {
-          print(a);
-          a++;
+          setState(() {
+            a++;
+          });
         },
       ),
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("연락처앱"),
+      ),
       bottomNavigationBar: BottomAppBar(),
       body: ListView.builder(
           itemCount: 3,
           itemBuilder: (c, i) {
             return ListTile(
-              leading: Image.asset("assets/cat1.jpeg"),
-              title: Text("홍길동"),
+              leading: CircleAvatar(child: Text(likes[i].toString())),
+              title: Text(name[i]),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.favorite_rounded,
+                  color: Colors.pink,
+                ),
+                onPressed: () {
+                  setState(() {
+                    likes[i] += 1;
+                  });
+                },
+              ),
             );
           }),
     ));
